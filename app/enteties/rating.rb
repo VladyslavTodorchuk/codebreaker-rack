44a202ddd8aacd03cb6c05e9cdee408365ab147d
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-08-18 15:39:21 +0300

    Add changes to Rating class

--- app/enteties/rating.rb
+++ app/enteties/rating.rb
@@ -8,31 +8,30 @@ module Entities
       @rating = []
     end
 
+    def all_games
+      sort_games({ easy: easy_games, medium: medium_games, hell: hell_games }, 3)
+    end
+
     def hell_games
-      @rating.select { |hash| hash[:difficulty] == 'hell' }
+      rating.select { |hash| hash[:difficulty] == 'hell' }
     end
 
     def medium_games
-      @rating.select { |hash| hash[:difficulty] == 'medium' }
+      rating.select { |hash| hash[:difficulty] == 'medium' }
     end
 
     def easy_games
-      @rating.select { |hash| hash[:difficulty] == 'easy' }
+      rating.select { |hash| hash[:difficulty] == 'easy' }
     end
 
-    def sort_games(games, top_rating = 1)
-      games = games.sort_by! do |elem|
-        [elem[:used_attempts], elem[:used_hints], elem[:name]]
+    def sort_games(games_hash, top_rating = 1)
+      games_hash.each do |difficulty, games|
+        sorted_games = games.sort_by! do |elem|
+          [elem[:used_attempts], elem[:used_hints], elem[:user]]
+        end
+        games_hash[difficulty] = sorted_games.uniq { |u| [u[:user]] }.first(top_rating)
       end
-      games.uniq { |u| u[:name] }.first(top_rating)
-    end
-
-    def all_games
-      easy = sort_games(easy_games, 3)
-      medium = sort_games(medium_games, 3)
-      hell = sort_games(hell_games, 3)
-
-      hell + medium + easy
+      games_hash
     end
   end
 end
